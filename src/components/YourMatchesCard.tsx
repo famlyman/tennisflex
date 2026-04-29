084bba7e4d0dd3f1d52b56c695a4a4cf636b4092
famlyman <burkecustomcomputers@gmail.com>
2026-04-29 10:59:24 -0400

    Build error/typo fix

--- src/components/YourMatchesCard.tsx
+++ src/components/YourMatchesCard.tsx
@@ -321,9 +321,8 @@ export default function YourMatchesCard({ matches, playerId }: YourMatchesCardPr
                         className="w-full px-3 py-2 text-slate-900 border border-slate-300 rounded-lg focus:outline-none focus:ring-2 focus:ring-indigo-500"
                         rows={3}
                       />
-                    </div>
-                  </div>
-                  </div>
+                     </div>
+                   </div>
 
                   <div className="flex gap-3">
                     <button
